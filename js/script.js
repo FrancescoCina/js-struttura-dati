1f62023e49bdf08b64358b1dd56baf2c2df64b67
FrancescoCina <francesco.cina04@gmail.com>
2021-07-23 16:30:30 +0200

    Creato Array Deck e stampato in HTML

--- js/script.js
+++ js/script.js
@@ -1,46 +1,6 @@
-const displayCard = document.getElementById("magic-card");
-
-
-var card = {
-    borderCard: "#000",
-    nome: "Archangel of Thune",
-    launchCost: ["3", "W", "W"],
-    typeOf: "Creature",
-    kindOf: "Angel",
-    expansion: {
-        nameOfExpansion: "Double Masters",
-        rarity: "Golden",
-        reprintId: 1,
-        collectionNr: "005",
-        totalCards: "332",
-    },
-    abilities: {
-        description: "Flying, lifelink. Whenever you gain life, put a +1/+1 counter on each creature you control.",
-        flavorText: "Even the wicked have nightmares.",
-    },
-    illustration: {
-        author: "James Ryman",
-        linkPic: "img/pic.jpg"
-    },
-    strenght: 3,
-    constitucion: 4,
-    backgroundCard: "beige",
-}
-
-console.table(card);
-
-
-// TO PRINT IN HTML
-
-let cardToPrint = "";
-
-
-displayCard.innerHTML = generateTemplateToPrint(card);
-
-
 // FUNCTION DECLARATION
 
-function generateTemplateToPrint(card) {
+const generateTemplateToPrint = (card) => {
 
     // Conditions for printing
 
@@ -60,7 +20,7 @@ function generateTemplateToPrint(card) {
 
     if (card.abilities.flavorText) {
         contentAbilities += `
-                            <li>Testo di Colore: ${card.abilities.flavorText}</li>                      
+                            <li>Testo di Colore: <em>${card.abilities.flavorText}</em></li>                      
                             `;
     }
     contentAbilities += `</ul>`
@@ -86,10 +46,115 @@ function generateTemplateToPrint(card) {
     <li class="list-group-item">Illustrazione della Carta:
         <ul>
             <li>Nome dell'autore: ${card.illustration.author}</li>
-            <li>Immagine dell'illustrazione: <div class="img-fluid"><img src="${card.illustration.linkPic}"></div></li>    
+            <li>Immagine dell'illustrazione: <div class="img-container img-fluid"><img src="${card.illustration.linkPic}"></div></li>    
         </ul>  
     </li>
 </ul>
 `;
     return cardToPrint;
 }
+
+// GET ELEMENT FROM HTML
+const displayCard = document.getElementById("magic-card");
+
+// ARRAY "DECK" of OBJECT "CARD"
+const deck = [{
+    id: 1,
+    borderCard: "#000",
+    nome: "Archangel of Thune",
+    launchCost: ["3", "W", "W"],
+    typeOf: "Creature",
+    kindOf: "Angel",
+    expansion: {
+        nameOfExpansion: "Double Masters",
+        rarity: "Golden",
+        reprintId: 1,
+        collectionNr: "5",
+        totalCards: "332",
+    },
+    abilities: {
+        description: "Flying, lifelink. Whenever you gain life, put a +1/+1 counter on each creature you control.",
+        flavorText: "Even the wicked have nightmares.",
+    },
+    illustration: {
+        author: "James Ryman",
+        linkPic: "img/pic_test.jpg"
+    },
+    strenght: 3,
+    constitucion: 4,
+    backgroundCard: "beige",
+}, {
+    id: 2,
+    borderCard: "#000",
+    nome: "Arborea Pegasus",
+    launchCost: ["3", "W"],
+    typeOf: "Creature",
+    kindOf: "Pegasus",
+    expansion: {
+        nameOfExpansion: "Adventures in the Forgotten Realms",
+        rarity: "Common",
+        reprintId: 1,
+        collectionNr: "2",
+        totalCards: "281",
+    },
+    abilities: {
+        description: "When Arborea Pegasus enters the battlefield, target creature gets +1/+1 and gains flying until end of turn",
+        flavorText: "The noble steeds of the elven gods can sometimes be persuaded to allow mere mortals on thei backs",
+    },
+    illustration: {
+        author: "Justyna Gil",
+        linkPic: "img/pic_2_test.jpg"
+    },
+    strenght: 2,
+    constitucion: 3,
+    backgroundCard: "beige",
+}, {
+    id: 3,
+    borderCard: "#000",
+    nome: "Devoted Paladin",
+    launchCost: ["4", "W"],
+    typeOf: "Creature",
+    kindOf: "Orc Knight",
+    expansion: {
+        nameOfExpansion: "Adventures in the Forgotten Realms",
+        rarity: "Common",
+        reprintId: 1,
+        collectionNr: "11",
+        totalCards: "281",
+    },
+    abilities: {
+        description: "Beacon of Hope -- When Devoted Paladin enter the battlefield, creatures you control get +1/+1 and gain vigilance untile end of turn.",
+        flavorText: "I swore a sacred oath to protect the weak and punish those who treathen them. As long as I draw breath, I will protect you.",
+    },
+    illustration: {
+        author: "Chris Ralles",
+        linkPic: "img/pic_3_test.jpg"
+    },
+    strenght: 4,
+    constitucion: 4,
+    backgroundCard: "beige",
+}]
+
+console.table(deck);
+
+
+// TO PRINT IN HTML
+
+let cardToPrint = "";
+
+/* displayCard.innerHTML = generateTemplateToPrint(card); */
+
+
+
+// TO PRINT ARRAY IN HTML
+
+const generateDeckTemplate = (deck) => {
+    let deckTemplate = "";
+    for (var i = 0; i < deck.length; i++) {
+        let currentCard = deck[i];
+        deckTemplate += generateTemplateToPrint(currentCard);
+    }
+    return deckTemplate;
+}
+
+displayCard.innerHTML = generateDeckTemplate(deck);
